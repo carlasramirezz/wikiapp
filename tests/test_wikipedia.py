43deb79baf0e50aa729723892c76a84705e3588b
Carla Sofia Ramirez <carlasofiaramirez@Carlas-MacBook-Air.local>
2024-05-29 16:39:18 -0400

    Added some docs

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,5 +1,8 @@
 from unittest.mock import Mock
 
+from click.exceptions import ClickException
+import pytest
+
 from wikiapp import wikipedia
 
 
@@ -7,3 +10,15 @@ def test_random_page_uses_given_language(mock_requests_get: Mock) -> None:
     wikipedia.random_page(language="es")
     args, _ = mock_requests_get.call_args
     assert "es.wikipedia.org" in args[0]
+
+
+@pytest.mark.e2e
+def test_random_page_returns_dictionary_in_production() -> None:
+    page = wikipedia.random_page()
+    assert isinstance(page, wikipedia.Page)
+
+
+def test_random_page_handles_validation_error(mock_requests_get: Mock) -> None:
+    mock_requests_get.return_value.__enter__.return_value.json.return_value = {}
+    with pytest.raises(ClickException):
+        wikipedia.random_page()
--- wikiapp/wikipedia.py
+++ wikiapp/wikipedia.py
@@ -3,17 +3,29 @@ from typing import Any
 import click
 import requests
 
+from pydantic import BaseModel, ValidationError
+
 API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page(language: str = "en") -> Any:
+class Page(BaseModel):
+    "A page dataclass to represent the return value from Wikipedia"
+
+    title: str  # Title of Wikipedia page
+    extract: str  # Extract of Wikipedia page
+
+
+def random_page(
+    language: str = "en",  # The language you want to use, as a two character string.
+) -> Page:  # Returns a page object.
     """Get a random page from Wikipedia."""
     try:
         with requests.get(API_URL.format(language=language), timeout=10) as response:
             response.raise_for_status()
             data = response.json()
+            return Page(**data)
         return data
-    except requests.RequestException as error:
+    except (requests.RequestException, ValidationError) as error:
         message = str(error)
         raise click.ClickException(message) from error
 
